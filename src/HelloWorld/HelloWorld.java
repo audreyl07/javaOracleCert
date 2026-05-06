f63010f730f920b024992735a24cac72b0cf7944
audreyl07 <audreylai07@gmail.com>
2026-05-06 14:37:48 -0400

    section 2

---
+++ src/HelloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+package HelloWorld;
+
+public class HelloWorld {
+    //main method
+    public static void main(String[] args) {
+        //executable, invokes all the other methods
+        System.out.println("Hello World"); //prints expression on console
+
+    }
+}
