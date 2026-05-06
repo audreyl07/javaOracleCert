06fa5886d987fd954e4fc6d833f2c338f276dc61
audreyl07 <audreylai07@gmail.com>
2026-05-06 16:25:18 -0400

    section 3

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -1,10 +1,10 @@
 package HelloWorld;
 
-public class HelloWorld {
+public class HelloWorld { //class definition
     //main method
-    public static void main(String[] args) {
+    public static void main(String[] args) { // access modifier + doesn't return value + name of method + arguments
         //executable, invokes all the other methods
         System.out.println("Hello World"); //prints expression on console
-
+        System.out.println("size of args = " + args.length);
     }
 }
