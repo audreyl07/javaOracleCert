868a766005dc067eec39f4778f35834862922ee7
audreyl07 <audreylai07@gmail.com>
2026-05-06 16:27:36 -0400

    section 3

---
+++ src/BasicsS3/JavaComments.java
@@ -0,0 +1,25 @@
+package BasicsS3;
+
+public class JavaComments {
+    /**
+     *
+     * @param args
+     * Defines the purpose of the method or the class
+     */
+    public static void main(String[] args) {
+        //Single line
+        //prints out a statement
+        System.out.println("Hello Java developers");
+
+        /*
+         * Multi line comment
+         * This usually allows the compiler to ignore a block of comments
+         * */
+        int x = 4;
+        int y=3;
+        int z=0;
+        z = x+y;
+        System.out.println("z= " + z);
+
+    }
+}
