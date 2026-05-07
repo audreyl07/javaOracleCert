c6a44927ae185b3fa9c6117935e505d246dbd814
Audrey <audreylai07@gmail.com>
2026-05-07 15:35:02 -0400

    new lessons

---
+++ src/BasicsS3/TypeCasting.java
@@ -0,0 +1,21 @@
+package BasicsS3;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        //implicit casting (conversion)
+        int a = 124;
+        long b = a; //implicit casting (widening)
+        double d = b;
+        System.out.println("Int value: "+a);
+        System.out.println("Int value: "+b);
+        System.out.println("Int value: "+d);
+
+        //explicit casting (type casting)
+        double dbl = 135.35;
+        long lng = (long)dbl; //explicit casting (narrowing)
+        byte byt = (byte)lng;
+        System.out.println("Double value: "+dbl);
+        System.out.println("Double value: "+lng);
+        System.out.println("Double value: "+byt);
+    }
+}
