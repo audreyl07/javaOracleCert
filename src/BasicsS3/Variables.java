df085b661ef6e7606378b5155983cbfa2db33f7f
audreyl07 <audreylai07@gmail.com>
2026-05-11 13:21:59 -0400

    section 3

---
+++ src/BasicsS3/Variables.java
@@ -0,0 +1,21 @@
+package BasicsS3;
+
+public class Variables {
+    public static void main(String[] args) {
+        int a = 10, b = 20,c = a + b;
+
+        System.out.println("c = " + c);
+
+        int age; //valid
+        int
+        /*
+        int a ge; //invalid
+        int 3age; //invalid
+
+        int break; //invalid
+
+         */
+
+    }
+
+}
