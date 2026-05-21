55c7afa4e20954dcd4e5d5fec609935369f29ddc
Audrey <audreylai07@gmail.com>
2026-05-21 15:27:28 -0400

    new section files

---
+++ src/JavaOOPS8/LocalInnerClass.java
@@ -0,0 +1,37 @@
+package JavaOOPS8;
+
+import java.util.Scanner;
+
+public class LocalInnerClass {
+    //classes defined in a block of code, such as a method, constructor, or even an if statement, are called local inner classes.
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = in.nextInt();
+        checkNumber(num);
+        in.close();
+    }
+
+    public static void checkNumber(int enteredNum){
+        int result = 0;
+
+        class NumberChecker{
+            boolean check;
+            public NumberChecker(int number){
+                check = number % 2 == result ? true : false;
+            }
+
+            public void printNumber(){
+                System.out.println("You entered: " + enteredNum);
+            }
+        }
+
+        NumberChecker checker = new NumberChecker(enteredNum);
+        checker.printNumber();
+        if (checker.check){
+            System.out.println(enteredNum + " is an even number.");
+        } else {
+            System.out.println(enteredNum + " is an odd number.");
+        }
+    }
+}
---
+++ src/JavaOOPS8/StaticNested.java
@@ -0,0 +1,8 @@
+package JavaOOPS8;
+
+public class StaticNested {
+    public static void main(String[] args) {
+        Nested.staticPerson person = new Nested.staticPerson();
+        person.show();
+    }
+}
